3bc4e174b6368a62f7a94d9eb79b3a7ece0d203c
User <gurchiani27@gmail.com>
2024-05-16 00:30:28 +0400

    my java

---
+++ main.js
@@ -0,0 +1,61 @@
+// const a = 23
+// const b = 24
+// if (a+b===47){
+//     console.log (true)
+// }
+// else{
+//     console.log (false)
+// }
+// function calculate (a, b) {
+//     return (a+b)
+// } 
+// console.log (calculate (23, 24))
+
+
+// function fahrenheittoCelsius (Temperature){
+//     if (typeof Temperature === "number") {
+//         return false
+//     }
+//     let Celsius=(Temperature -32)* 5/9
+//     return Celsius
+// } 
+    
+
+
+
+
+
+// const a = 15
+// const b = "15"
+// if (a===b) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+
+
+
+
+// const a = 5;
+// const b = 6;
+// function calculate(a, b) {
+//     if (typeof a === 'string') {
+//         return false
+//     }
+//     if ( typeof b === 'string'){
+//         return f
+//     }
+//     const result = a * b  
+//     if (result === 30) {
+//         console.log("The product is 30")
+//     }
+//     return result
+// }
+// const result = calculate(a, b);
+// console.log(result); 
+
+    
+
+    
+    
